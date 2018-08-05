0ada8e77d9e88038f970c1ff95cb5f8f67ed3ef9
Charles O'Farrell <charleso@charleso.org>
2018-08-06 07:27:05 +1000

    Support companion object properties

--- runner/src/main/scala/hedgehog/runner/Properties.scala
+++ runner/src/main/scala/hedgehog/runner/Properties.scala
@@ -1,10 +1,55 @@
 package hedgehog.runner
 
 import hedgehog._
+import hedgehog.core._
 
 abstract class Properties {
 
   def tests: List[Prop]
+
+  /** Allows the implementing test to be run separately without SBT */
+  def main(args: Array[String]): Unit = {
+    val seed = Seed.fromTime()
+    tests.foreach(t => {
+      val report = t.result.check(seed).value
+      println(Prop.renderReport(this.getClass.getName, t, report, ansiCodesSupported = true))
+    })
+  }
 }
 
 case class Prop(name: String, result: Property[Unit])
+
+object Prop {
+
+  def renderReport(className: String, t: Prop, report: Report, ansiCodesSupported: Boolean): String = {
+    def render(ok: Boolean, msg: String, extraS: List[String]): String = {
+      val name = className + "." + t.name
+      val sym = if (ok) "+" else "-"
+      val colour = if (ok) Console.GREEN else Console.RED
+      val extra = if (extraS.isEmpty) "" else "\n" + extraS.map(s => "> " + s).mkString("\n")
+      if(ansiCodesSupported) {
+        s"$colour$sym${Console.RESET} $name: $msg$extra"
+      } else {
+        s"$sym $name: $msg$extra"
+      }
+    }
+
+    report.status match {
+      case Failed(shrinks, log) =>
+        render(false, s"Falsified after ${report.tests.value} passed tests", log.map(renderLog))
+      case GaveUp =>
+        render(false, s"Gave up after only ${report.tests.value} passed test. " +
+          s"${report.discards.value} were discarded", Nil)
+      case OK =>
+        render(true, s"OK, passed ${report.tests.value} tests", Nil)
+    }
+  }
+
+  def renderLog(log: Log): String =
+    log match {
+      case ForAll(name, value) =>
+        s"${name.value}: $value"
+      case Info(value) =>
+        value
+    }
+}
