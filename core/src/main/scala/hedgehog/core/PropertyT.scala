3297488a0bab5ae90e5f0ad6333e1375ea8dbcd2
Charles O'Farrell <charleso@charleso.org>
2018-09-22 16:22:38 +1000

    Add property config

--- core/src/main/scala/hedgehog/core/PropertyT.scala
+++ core/src/main/scala/hedgehog/core/PropertyT.scala
@@ -16,13 +16,28 @@ sealed trait Log
 case class ForAll(name: Name, value: String) extends Log
 case class Info(value: String) extends Log
 
-case class PropertyT[M[_], A](run: GenT[M, (List[Log], Option[A])]) {
+case class PropertyConfig(
+    testLimit: SuccessCount
+  , discardLimit: DiscardCount
+  , shrinkLimit: ShrinkLimit
+  )
+
+object PropertyConfig {
+
+  def default: PropertyConfig =
+    PropertyConfig(SuccessCount(100), DiscardCount(100), ShrinkLimit(1000))
+}
+
+case class PropertyT[M[_], A](
+    config: PropertyConfig
+  , run: GenT[M, (List[Log], Option[A])]
+  ) {
 
   def map[B](f: A => B)(implicit F: Functor[M]): PropertyT[M, B] =
-    PropertyT(run.map(x => x.copy(_2 = x._2.map(f))))
+    copy(run = run.map(x => x.copy(_2 = x._2.map(f))))
 
   def flatMap[B](f: A => PropertyT[M, B])(implicit F: Monad[M]): PropertyT[M, B] =
-    PropertyT(run.flatMap(x =>
+    copy(run = run.flatMap(x =>
       x._2.fold(
        GenT.GenApplicative(F).point((x._1, Option.empty[B]))
       )(
@@ -31,11 +46,14 @@ case class PropertyT[M[_], A](run: GenT[M, (List[Log], Option[A])]) {
     ))
 
   def check(seed: Seed)(implicit F: Monad[M]): M[Report] =
-    propertyT.report(SuccessCount(100), Size(0), seed, this)
+    propertyT.report(Size(0), seed, this)
 
   def checkRandom(p: PropertyT[M, Unit])(implicit F: Monad[M]): M[Report] =
     // FIX: predef MonadIO
     check(Seed.fromTime())
+
+ def withTests(n: SuccessCount):  PropertyT[M, A] =
+   copy(config = config.copy(testLimit = n))
 }
 
 object PropertyT {
@@ -56,32 +74,36 @@ trait PropertyTReporting[M[_]] {
   def isFailure[A, B](n: Node[M, Option[(B, Option[A])]]): Boolean =
     n.value.map(_._2) == Some(None)
 
-  def takeSmallest[A](n: ShrinkCount, t: Node[M, Option[(List[Log], Option[A])]])(implicit F: Monad[M]): M[Status] =
+  def takeSmallest[A](n: ShrinkCount, slimit: ShrinkLimit, t: Node[M, Option[(List[Log], Option[A])]])(implicit F: Monad[M]): M[Status] =
     t.value match {
       case None =>
         F.point(Status.gaveUp)
 
       case Some((w, None)) =>
-        F.map(findMapM(t.children)(m =>
-          F.bind(m.run)(node =>
-            if(isFailure(node))
-              F.map(takeSmallest(n.inc, node))(some)
-            else
-              F.point(Option.empty[Status])
-          )
-        ))(_.getOrElse(Status.failed(n, w)))
+        if (n.value >= slimit.value) {
+          F.point(Status.failed(n, w))
+        } else {
+          F.map(findMapM(t.children)(m =>
+            F.bind(m.run)(node =>
+              if(isFailure(node))
+                F.map(takeSmallest(n.inc, slimit, node))(some)
+              else
+                F.point(Option.empty[Status])
+            )
+          ))(_.getOrElse(Status.failed(n, w)))
+        }
 
       case Some((_, Some(_))) =>
         F.point(Status.ok)
     }
 
-  def report[A](n : SuccessCount, size0: Size, seed0: Seed, p: PropertyT[M, A])(implicit F: Monad[M]): M[Report] = {
+  def report[A](size0: Size, seed0: Seed, p: PropertyT[M, A])(implicit F: Monad[M]): M[Report] = {
     def loop(successes: SuccessCount, discards: DiscardCount, size: Size, seed: Seed): M[Report] =
       if (size.value > 99)
         loop(successes, discards, Size(0), seed)
-      else if (successes == n)
+      else if (successes.value >= p.config.testLimit.value)
         F.point(Report(successes, discards, OK))
-      else if (discards.value >= 100)
+      else if (discards.value >= p.config.discardLimit.value)
         F.point(Report(successes, discards, GaveUp))
       else
         F.bind(p.run.run(size, seed).run)(x =>
@@ -90,7 +112,7 @@ trait PropertyTReporting[M[_]] {
               loop(successes, discards.inc, size.inc, x.value._1)
 
             case Some((_, None)) =>
-              F.map(takeSmallest(ShrinkCount(0), x.map(_._2)))(y => Report(successes, discards, y))
+              F.map(takeSmallest(ShrinkCount(0), p.config.shrinkLimit, x.map(_._2)))(y => Report(successes, discards, y))
 
             case Some((m, Some(_))) =>
               loop(successes.inc, discards, size.inc, x.value._1)
@@ -100,7 +122,7 @@ trait PropertyTReporting[M[_]] {
   }
 
   def recheck(size: Size, seed: Seed)(p: PropertyT[M, Unit])(implicit F: Monad[M]): M[Report] =
-    report(SuccessCount(1), Size(0), seed, p)
+    report(Size(0), seed, p.withTests(SuccessCount(1)))
 }
 
 /**********************************************************************/
@@ -113,11 +135,20 @@ case class ShrinkCount(value: Int) {
     ShrinkCount(value + 1)
 }
 
+/** The number of shrinks to try before giving up on shrinking. */
+case class ShrinkLimit(value: Int)
+
 /** The number of tests a property ran successfully. */
 case class SuccessCount(value: Int) {
 
   def inc: SuccessCount =
     SuccessCount(value + 1)
+}
+
+object SuccessCount {
+
+  implicit def Int2SuccessCount(i: Int): SuccessCount =
+    SuccessCount(i)
 }
 
 /** The number of tests a property had to discard. */
