ef7f93bde6c8d122d82d71fa67b7f9c16eda031b
Kevin Lee <kevin.code@kevinlee.io>
2020-08-04 23:45:55 +1000

    Add: delay between Maven Central sync Remove: hedgehog-minitest from Maven Central sync temporarily to do it from Bintray web UI

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -5,18 +5,25 @@
 
 echo "  SCALA_VERSION=$SCALA_VERSION"
 echo "PROJECT_VERSION=$PROJECT_VERSION"
+echo "   BINTRAY_REPO=$BINTRAY_REPO"
+
+echo "sbt publish no maven style to $BINTRAY_REPO"
 
 sbt ++${SCALA_VERSION}! \
   'set version in ThisBuild := "'$PROJECT_VERSION'"' \
   "set publishMavenStyle in ThisBuild := false" \
   publish
+
+echo "sbt publish maven style to $BINTRAY_REPO"
 # Keep publishing to the "generic" bintray repository
 sbt ++${SCALA_VERSION}! \
   'set version in ThisBuild := "'$PROJECT_VERSION'"' \
   "set publishMavenStyle in ThisBuild := true" \
   publish
-# Publish to the "maven" bintray repository as well
+
 export BINTRAY_REPO=${BINTRAY_REPO:-scala-hedgehog-maven}
+echo "sbt publish maven style to $BINTRAY_REPO"
+# Publish to the "maven" bintray repository as well
 sbt ++${SCALA_VERSION}! \
   'set version in ThisBuild := "'$PROJECT_VERSION'"' \
   'set publishMavenStyle in ThisBuild := true' \
