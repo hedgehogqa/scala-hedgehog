b650f1763041ba28a677c5b29ecebcb502b753a9
Kevin Lee <kevin.code@kevinlee.io>
2024-09-01 22:17:35 +1000

    Update: CoverageTest example

--- example/shared/src/main/scala/hedgehog/examples/CoverageTest.scala
+++ example/shared/src/main/scala/hedgehog/examples/CoverageTest.scala
@@ -8,9 +8,13 @@ object CoverageTest extends Properties {
   override def tests: List[Test] =
     List(
       property("collect", testCollect)
-    , property("classify", testClassify)
-    , property("cover number (expected to fail)", testCoverNumber)
-    , property("cover boolean", testCoverBoolean)
+      , property("collect 2", testCollect2)
+      , property("classify", testClassify)
+      , property("classify 2", testClassify2)
+      , property("cover number (expected to fail)", testCoverNumber)
+      , property("cover number 2 (expected to fail)", testCoverNumber2)
+      , property("cover boolean", testCoverBoolean)
+      , property("cover boolean 2", testCoverBoolean2)
     )
 
   def testCollect: Property =
@@ -18,11 +22,25 @@ object CoverageTest extends Properties {
       _ <- Gen.int(Range.linear(1, 10)).forAll.collect
     } yield Result.success
 
+  def testCollect2: Property =
+    for {
+      _ <- Gen.int(Range.linear(1, 10)).log("n").collect
+      _ <- Gen.string(Gen.char('a', 'c'), Range.singleton(1)).log("s").collect
+    } yield Result.success
+
   def testClassify: Property =
     for {
       _ <- Gen.int(Range.linear(1, 100)).forAll
         .classify("small number", _ < 50)
         .classify("large number", _ >= 50)
+    } yield Result.success
+
+  def testClassify2: Property =
+    for {
+      _ <- Gen.int(Range.linear(1, 100)).log("n")
+        .classify("small number", _ < 50)
+        .classify("large number", _ >= 50)
+      _ <- Gen.int(Range.linear(1, 100)).log("m")
     } yield Result.success
 
   def testCoverNumber: Property =
@@ -33,9 +51,24 @@ object CoverageTest extends Properties {
         .cover(5, "big number", _ >= 70)
     } yield Result.success
 
+  def testCoverNumber2: Property =
+    for {
+      _ <- Gen.int(Range.linear(1, 100)).log("n")
+        .cover(50, "small number", _ < 10)
+        .cover(15, "medium number", _ >= 20)
+        .cover(5, "big number", _ >= 70)
+    } yield Result.success
+
   def testCoverBoolean: Property =
     for {
       _ <- Gen.boolean.forAll
+        .cover(30, "true", x => x)
+        .cover(30, "false", x => !x)
+    } yield Result.success
+
+  def testCoverBoolean2: Property =
+    for {
+      _ <- Gen.boolean.log("b")
         .cover(30, "true", x => x)
         .cover(30, "false", x => !x)
     } yield Result.success
